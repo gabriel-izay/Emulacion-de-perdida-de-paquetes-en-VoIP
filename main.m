3f9f2ab5cd70924e8814946f14702e5a39501448
Ian Gabriel Gonzalez Zayas <55033183+gabriel-izay@users.noreply.github.com>
2020-06-16 00:24:04 -0300

    Add files via upload

--- main.m
+++ main.m
@@ -1,51 +1,22 @@
-% Se selecciona un archivo de audio
-% Se lee el archivo y genera un vector
-% [file,path] = uigetfile('*.wav');
-% if isequal(file,0)
-%     disp('Operación cancelada');
-% else
-%     disp(['Archivo seleccionado: ', fullfile(path,file)]);
-%     pathfile = strcat(path,file);
-%     [x, fm] = audioread(pathfile);
-%     %x = {x};
-%     x=x';
-%     y=x;
-%     filename = {file};
-% end
-[x, fm] = audioread('Originales\Vocal.wav');
-x=x';
-% Se indica un porcentaje de perdida de muestras
-%losspc=input('Ingrese un porcentaje de perdidas: ');
-% Se determina, segun sample rate y duracion del audio,
-% cuantas muestras se pierden
-samplesOrig=length(x);
-mask=randn(1,samplesOrig);
-mask=(mask/max(abs(mask)));
-mask2=mask;
-% for i=1:length(mask)
-%     mask2(i)=abs(round((mask(i))));
-%     if mask2(i)>1
-%         mask2(i)=1;
-%     end
-% end
-patron_perdidas=[0 0 0 0 0 1 1 0 0 1 0 1 0 1 0 1 1 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0];
-mascaraMarkov2=zeros(1,length(patron_perdidas));
-for i=1:length(patron_perdidas)
-    if patron_perdidas(i)==0
-        mascaraMarkov2(i)=1;
-    end
-end
-auxsamples=find(mask2);
-samplesrep=length(auxsamples);
-lostsamples=samplesOrig-samplesrep;
-disp('Samples de audio original: ',samplesOrig);
-disp('Samples a reproducir: ',samplesrep);
-disp('Samples perdidos: ',lostsamples)
-%audio and punto a punto mscara
-% audioloss=x.*mask2;
-% %ploteo de audio mas audio con perdidas
-% subplot(2,1,1)
-% plot(x)
-% subplot(2,1,2)
-% plot(audioloss)
+%% Selección de archivo de audio .wav
+
+    [file,path] = uigetfile('*.wav');
+    disp(['Archivo seleccionado: ', fullfile(path,file)]);
+    pathfile = strcat(path,file);
+    [x, fm] = audioread(pathfile);
+    x=x';
+    filename = {file};
+    %Datos del audio
+    %Cantidad de muestras del audio original, fm:frecuencia de muestreo
+    nsamplesOrig=length(x);
+    
+%% Patrón de pérdidas, se genera vector máscara binaria
+
+    mascara_mkov=generarMascara(nsamplesOrig);
+    
+%% Producto punto a punto entre la mascara y el audio
+
+    audiowloss=x.*mascara_mkov;
+
+
 
